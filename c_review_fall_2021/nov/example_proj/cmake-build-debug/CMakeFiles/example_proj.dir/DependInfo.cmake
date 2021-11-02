9dabe0e9b2a8eb5d2d48a8f91ea2739c4ff0cbb4
Abdullah Almosalami <abdullah.almosalami@ndus.edu>
2021-11-01 19:47:23 -0500

    Update 11-01-2021

---
+++ c_review_fall_2021/nov/example_proj/cmake-build-debug/CMakeFiles/example_proj.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/git/CPractice/c_review_fall_2021/nov/example_proj/main.c" "C:/git/CPractice/c_review_fall_2021/nov/example_proj/cmake-build-debug/CMakeFiles/example_proj.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ c_review_fall_2021/nov/example_proj/cmake-build-debug/CMakeFiles/example_proj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example_proj.dir/main.c.obj"
+  "example_proj.exe"
+  "example_proj.exe.manifest"
+  "example_proj.pdb"
+  "libexample_proj.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/example_proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
