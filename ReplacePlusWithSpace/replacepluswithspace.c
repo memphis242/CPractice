e6287c257c277fd287d9702a7d836657344a824d
C85HUP8 <AlmosalamiAbdullahI@JohnDeere.com>
2023-10-23 13:20:02 -0500

    Started replacepluswithspace program.
    Intent of program is to call it anywhere from a terminal program (so I'll this spot to my PATH variable) and pass into it a relative or absolute filepath to rename a file if it has any '+' characters with the ' ' (space) character. I want to do this in C so that I can invoke this from anywhere instead of Python where I have to run the program by calling Python and specifying the full-path of the Python script. There's probably other workarounds but I thought having a C way to do this would be fun.

---
+++ ReplacePlusWithSpace/replacepluswithspace.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define STRING_MAX_SIZE 150
+ 
+int main(int argc, char *argv[])
+{
+   char * myFileName;
+   bool isIndexAPlus[STRING_MAX_SIZE] =   { 
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          false, false, false, false, false, false, false, false, false, false,
+                                          };
+   char newFileName[STRING_MAX_SIZE + 1];
+   int ret = 1;
+
+   // Read in file-name argument
+   myFileName = argv[1];
+
+   // Find all the pluses
+   for ( unsigned int i=0; i<STRING_MAX_SIZE; i++ )
+   {
+      if ( myFileName[i] == '+' )
+      {
+         isIndexAPlus[i] = true;
+      }
+   }
+
+   // Create new string with pluses replaced by spaces
+   for ( unsigned int i=0; i<STRING_MAX_SIZE; i++ )
+   {
+      if ( isIndexAPlus[i] == true )
+      {
+         newFileName[i] = ' ';
+      }
+      else
+      {
+         newFileName[i] = myFileName[i];
+      }
+   }
+   newFileName[STRING_MAX_SIZE + 1] = '\0';
+
+   // Replace filename
+   ret = replace( myFileName, newFileName );
+   if ( ret == 0 )
+   {
+      printf("File name changed successfully.");
+   }
+   else
+   {
+      printf("FAILED to rename file.");
+   }
+}
