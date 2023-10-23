2c0430a4061810161c8e9038b446b44700ab016b
C85HUP8 <AlmosalamiAbdullahI@JohnDeere.com>
2023-10-23 13:36:29 -0500

    Sketchpad commit.

--- work/general_sketchpad/main.c
+++ work/general_sketchpad/main.c
@@ -7,25 +7,38 @@ struct Data_S
     uint8_t data[8];
 };
 
-static size_t LengthOfData( const struct Data_S * data_ptr );
+static uint8_t AccessMyArray_UsingPointerToPrimitive( const uint8_t * data_ptr );
+static uint8_t AccessMyArray_UsingPointerToStruct( const struct Data_S * data_ptr );
+
+static const uint8_t Example[] = {0, 1, 2};
+static const uint8_t Example2[2] = {0, 1, 2};
 
 int main(void)
 {
     uint8_t data[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
     struct Data_S data_struct;
 
-    for ( uint8_t i=0; i<8; i++ )
+    for ( uint8_t i=0; i<12; i++ )
     {
         data_struct.data[i] = data[i];
     }
 
-    printf( "Length of my data array: %d\n", LengthOfData(&data_struct) );
+    data[10] = 0;
+    data_struct.data[10] = 0;
+
+    printf("%d", AccessMyArray_UsingPointerToStruct( &data_struct ) );
+    printf("%d", AccessMyArray_UsingPointerToPrimitive( data ) );
 
     return 0;
 }
 
 
-static size_t LengthOfData( const struct Data_S * data_ptr )
+static uint8_t AccessMyArray_UsingPointerToPrimitive( const uint8_t * data_ptr )
 {
-    return sizeof( *data_ptr );
+    return data_ptr[20];    // Intentionally obvious out-of-bounds array access
+}
+
+static uint8_t AccessMyArray_UsingPointerToStruct( const struct Data_S * data_ptr )
+{
+    return data_ptr->data[20];    // Intentionally obvious out-of-bounds array access
 }
